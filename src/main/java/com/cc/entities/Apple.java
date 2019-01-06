3fb828f9e0c5ae338d0de0ad42bfaa0d23be1920
xiaogong110110 <371845698@qq.com>
2019-01-06 16:37:27 +0800

    forth times

--- src/main/java/com/cc/entities/Apple.java
+++ src/main/java/com/cc/entities/Apple.java
@@ -1,7 +1,7 @@
 package com.cc.entities;
 
 public class Apple {
-	private	String appName="good apple hahha";
+	private	String appName="good apple hahhaajjdkjf";
 	
 	public String getAppName() {
 		return appName;
