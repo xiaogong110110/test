21ca9c1a8ca8e931d0204ed3326e3c88d3bf4ef3
xiaogong110110 <371845698@qq.com>
2019-01-21 10:42:07 +0800

    13 times

--- src/main/java/com/cc/entities/Apple.java
+++ src/main/java/com/cc/entities/Apple.java
@@ -4,7 +4,7 @@ public class Apple {
 	private	String appName="good apple name";
 	
 	public String getAppName() {
-                                System.out.println("Hello World");
+                                System.out.println("Hello World22");
 		return appName;
 	}
 }
