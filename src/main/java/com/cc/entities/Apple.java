668f07fcdc1c8c150dd5612c1647dc9ef2628a32
xiaogong110110 <371845698@qq.com>
2019-01-21 14:20:56 +0800

    14times commit

--- src/main/java/com/cc/entities/Apple.java
+++ src/main/java/com/cc/entities/Apple.java
@@ -1,7 +1,7 @@
 package com.cc.entities;
 
 public class Apple {
-	private	String appName="good apple names";
+	private	String appName="good apple names!!!";
 	
 	public String getAppName() {
                                 System.out.println("Hello World2");
